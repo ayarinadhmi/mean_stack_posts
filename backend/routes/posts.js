a00a5cff74f853fc3efbab0fd2e7a7b0f380b28f
unknown <nadhmi.ayari@esprit.tn>
2020-08-04 07:06:34 +0100

    finalized pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const multer = require("multer");
-const router = express.Router();
+
 const Post = require("../model/post");
-const { isBuffer } = require("util");
+
+const router = express.Router();
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
-  "image/jpeg": "jpeg",
+  "image/jpeg": "jpg",
   "image/jpg": "jpg"
 };
 
@@ -39,76 +40,77 @@ router.post(
       content: req.body.content,
       imagePath: url + "/images/" + req.file.filename
     });
-    post.save().then(postData => {
+    post.save().then(createdPost => {
       res.status(201).json({
+        message: "Post added successfully",
         post: {
-          ...postData,
-          id: postData._id
-        },
-        message: "Post added successfully"
+          ...createdPost,
+          id: createdPost._id
+        }
       });
     });
   }
 );
 
+router.put(
+  "/:id",
+  multer({ storage: storage }).single("image"),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + "://" + req.get("host");
+      imagePath = url + "/images/" + req.file.filename;
+    }
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath
+    });
+    console.log(post);
+    Post.updateOne({ _id: req.params.id }, post).then(result => {
+      res.status(200).json({ message: "Update successful!" });
+    });
+  }
+);
+
 router.get("", (req, res, next) => {
-  //+ to convert them to numbers
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
   let fetchedPosts;
-
   if (pageSize && currentPage) {
-    postQuery.skip(pageSize * (currentPage + 1)).limit(pageSize);
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
-
-  postQuery.then(documents => {
-    fetchedPosts = documents;
-    return Post.count();
-  }).then(count => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: fetchedPosts,
-      maxPosts:count
+  postQuery
+    .then(documents => {
+      fetchedPosts = documents;
+      return Post.count();
     })
-
-  });
-  router.delete("/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-      res.status(200).json({ message: "post deleted successfully" });
+    .then(count => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: fetchedPosts,
+        maxPosts: count
+      });
     });
-  });
+});
 
-  router.put(
-    "/:id",
-    multer({ storage: storage }).single("image"),
-    (req, res, next) => {
-      let imagePath = req.body.imagePath;
-      if (req.file) {
-        const url = req.protocol + "://" + req.get("host");
-        imagePath = url + "/images/" + req.file.filename;
-      }
-      const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: imagePath
-      });
-      console.log(post);
-      Post.updateOne({ _id: req.params.id }, post).then(result => {
-        res.status(200).json({ message: "Update successful!" });
-      });
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
     }
-  );
-
-  router.get("/:id", (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "Post not found!" });
-      }
-    });
   });
+});
 
-  module.exports = router;
+router.delete("/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
+
+module.exports = router;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
-import { Post } from './post.model';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { longStackSupport } from 'q';
-import { Router } from '@angular/router';
+import { Post } from "./post.model";
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
-  private postsUpdated = new Subject<{posts:Post[],postCount:number}>();
+  private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts(postsPerPage: number, currentPage: number) {
-    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`
+    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string; posts: any, maxPosts: number }>('http://localhost:3000/api/posts')
+      .get<{ message: string; posts: any; maxPosts: number }>(
+        "http://localhost:3000/api/posts" + queryParams
+      )
       .pipe(
         map(postData => {
           return {
@@ -36,45 +36,15 @@ export class PostsService {
       )
       .subscribe(transformedPostData => {
         this.posts = transformedPostData.posts;
-        this.postsUpdated.next({posts:[...this.posts],postCount:transformedPostData.maxPosts});
+        this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: transformedPostData.maxPosts
+        });
       });
   }
 
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
-  }
-
-  addPost(title: string, content: string, image: File) {
-    const postData = new FormData();
-    postData.append('title', title);
-    postData.append('content', content);
-    postData.append('image', image, title);
-    this.http
-      .post<{ message: string; post: Post }>(
-        'http://localhost:3000/api/posts',
-        postData
-      )
-      .subscribe(responseData => {
-        const post: Post = {
-          id: responseData.post.id,
-          title: title,
-          content: content,
-          imagePath: responseData.post.imagePath
-        };
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
-        this.router.navigate(['/']);
-      });
-  }
-
-  deletePost(postId: any) {
-    this.http
-      .delete('http://localhost:3000/api/posts/' + postId)
-      .subscribe(() => {
-        const updatedPosts = this.posts.filter(p => p.id !== postId);
-        this.posts = updatedPosts;
-        this.postsUpdated.next([...this.posts]);
-      });
   }
 
   getPost(id: string) {
@@ -83,17 +53,32 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
-    }>('http://localhost:3000/api/posts/' + id);
+    }>("http://localhost:3000/api/posts/" + id);
+  }
+
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
+    this.http
+      .post<{ message: string; post: Post }>(
+        "http://localhost:3000/api/posts",
+        postData
+      )
+      .subscribe(responseData => {
+        this.router.navigate(["/"]);
+      });
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
     let postData: Post | FormData;
-    if (typeof image === 'object') {
+    if (typeof image === "object") {
       postData = new FormData();
-      postData.append('id', id);
-      postData.append('title', title);
-      postData.append('content', content);
-      postData.append('image', image, title);
+      postData.append("id", id);
+      postData.append("title", title);
+      postData.append("content", content);
+      postData.append("image", image, title);
     } else {
       postData = {
         id: id,
@@ -103,20 +88,14 @@ export class PostsService {
       };
     }
     this.http
-      .put('http://localhost:3000/api/posts/' + id, postData)
+      .put("http://localhost:3000/api/posts/" + id, postData)
       .subscribe(response => {
-        const updatedPosts = [...this.posts];
-        const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
-        const post: Post = {
-          id: id,
-          title: title,
-          content: content,
-          imagePath: ''
-        };
-        updatedPosts[oldPostIndex] = post;
-        this.posts = updatedPosts;
-        this.postsUpdated.next([...this.posts]);
-        this.router.navigate(['/']);
+        this.router.navigate(["/"]);
       });
+  }
+
+  deletePost(postId: string) {
+    return this.http
+      .delete("http://localhost:3000/api/posts/" + postId);
   }
 }
